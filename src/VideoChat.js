52039e8c3458bb44384c4fda325f671859359ff1
Surajit712306 <surajit03011999@gmail.com>
2021-07-04 02:28:22 +0530

    production

--- src/VideoChat.js
+++ src/VideoChat.js
@@ -7,11 +7,11 @@ const VideoChat = props => {
   return (
     <div className="app">
       <Switch>
-        <Route exact path="/">
-          <CreateRoom />
-        </Route>
         <Route path="/:roomId">
           <Room />
+        </Route>
+        <Route exact path="/">
+          <CreateRoom />
         </Route>
         <Redirect to="/" />
       </Switch>
